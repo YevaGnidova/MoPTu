db042423a6cbd3329537eea68efe0163b56eb379
eva <evagnidova@gmail.com>
2024-05-10 16:55:32 +0300

    v.0.5 new design

---
+++ app/src/main/java/com/example/moptu/fragments/CityBackFragment.kt
@@ -0,0 +1,4 @@
+package com.example.moptu.fragments
+
+class CityBackFragment {
+}
---
+++ app/src/main/java/com/example/moptu/fragments/CitySelectionFragment.kt
@@ -0,0 +1,11 @@
+package com.example.moptu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CitySelectionFragment : Fragment(R.layout.fragment_city_selection) {
+
+}
